70eb90296a24074c290b911a35e017a173c7fe1e
Triktron <ele.trik@hotmail.com>
2020-04-17 19:32:29 +0200

    added a texture to the ball and fixed the controller

---
+++ Assets/CatAnimationController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAnimationController : MonoBehaviour
+{
+    Animator m_Animator;
+    public Rigidbody rb;
+
+    public float IdleTime = 2;
+    float CurrentIdleTime;
+    
+    public AnimationCurve AnimationSpeedMultiplier = new AnimationCurve(new Keyframe(0f, 0f, 0f, 5f), new Keyframe(1f, 2f, 0f, 0f));
+
+    public float MaxSpeed = 0.5f;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Animator.SetFloat("WalkSpeed", AnimationSpeedMultiplier.Evaluate(rb.velocity.magnitude));
+
+
+        if (rb.velocity.magnitude > MaxSpeed)
+        {
+            m_Animator.ResetTrigger("Sit");
+            m_Animator.ResetTrigger("Idle");
+
+
+            m_Animator.SetTrigger("Walk");
+            CurrentIdleTime = 0;
+        }
+        else if (CurrentIdleTime < IdleTime)
+        {
+            m_Animator.ResetTrigger("Walk");
+            m_Animator.ResetTrigger("Sit");
+
+
+            m_Animator.SetTrigger("Idle");
+            CurrentIdleTime += Time.deltaTime;
+        }
+        else
+        {
+            m_Animator.ResetTrigger("Walk");
+            m_Animator.ResetTrigger("Idle");
+
+
+            m_Animator.SetTrigger("Sit");
+        }
+    }
+}
---
+++ Assets/ClonePositionAndHeading.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonePositionAndHeading : MonoBehaviour
+{
+    public Transform ObjectToFollow;
+    public Vector3 Offset;
+    public float MaxHeadingDistance = 0.1f;
+    public float lookSpeed = 10;
+
+    Vector3 LastHeading;
+    Vector3 _lookDirection;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         transform.position = ObjectToFollow.position - Offset;
+
+        if (Vector3.Distance(transform.position, LastHeading) > MaxHeadingDistance)
+        {
+            _lookDirection = transform.position - LastHeading;
+
+            LastHeading = transform.position;
+        }
+
+        Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y,0);
+    }
+}
