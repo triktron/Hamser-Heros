96170b48262dd7d01c2f77cb35faa019369c005d
Triktron <ele.trik@hotmail.com>
2020-04-17 18:17:02 +0200

    added ball controler and camera folower

---
+++ Assets/Scripts/Camera/CameraFollowController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowController : MonoBehaviour
+{
+	public Transform objectToFollow;
+	public Vector3 offset;
+	public float followSpeed = 10;
+	public float lookSpeed = 10;
+
+	public enum Modes
+	{
+		Default,
+		WorldStatic,
+		World,
+		PulledString,
+	}
+
+	public Modes Mode;
+
+	Vector3 LastTargetPos;
+
+	private void Awake()
+	{
+		LastTargetPos = objectToFollow.position + Vector3.up * offset.y;
+	}
+
+	public void LookAtTarget()
+	{
+		Vector3 _lookDirection = objectToFollow.position - transform.position;
+		Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
+		transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
+	}
+
+	public void MoveToTarget()
+	{
+
+		Vector3 _targetPos = offset;
+
+		if (Mode == Modes.World)
+		{
+			_targetPos = objectToFollow.position + offset;
+		}
+		if (Mode == Modes.PulledString)
+		{
+			_targetPos = LastTargetPos;
+
+			Vector3 target = objectToFollow.position + Vector3.up * offset.y;
+			float distance = Vector3.Distance(objectToFollow.position + Vector3.up * offset.y, LastTargetPos);
+
+			_targetPos = Vector3.MoveTowards(LastTargetPos, target, distance - offset.x);
+		}
+		else if (Mode == Modes.Default)
+		{
+			_targetPos = objectToFollow.position +
+								objectToFollow.forward * offset.z +
+								objectToFollow.right * offset.x +
+								objectToFollow.up * offset.y;
+
+		}
+
+
+		Debug.DrawLine(objectToFollow.position, _targetPos);
+		transform.position = Vector3.Lerp(transform.position, _targetPos, followSpeed * Time.deltaTime);
+		LastTargetPos = _targetPos;
+	}
+
+	private void FixedUpdate()
+	{
+		LookAtTarget();
+		MoveToTarget();
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 100;
+
+    Vector3 Movement;
+    Rigidbody rb;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        float angle = Camera.main.transform.rotation.eulerAngles.y;
+
+        Vector3 forward = new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), 0, Mathf.Cos(Mathf.Deg2Rad * angle));
+        Vector3 left = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), 0, -Mathf.Sin(Mathf.Deg2Rad * angle));
+
+
+        Movement = Input.GetAxis("Horizontal") * left + Input.GetAxis("Vertical") * forward;
+
+        Debug.DrawRay(transform.position, forward * 2, Color.red);
+        Debug.DrawRay(transform.position, left * 2, Color.green);
+        Debug.DrawRay(transform.position, Movement * 2, Color.magenta);
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(Movement * Speed);
+    }
+}
