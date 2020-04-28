4932d9a4dde253ed0edc0de7cd5999e6c5458910
triktron <ele.trik@hotmail.com>
2020-04-28 16:37:21 +0200

    added sounds and fixed some bugs

---
+++ Assets/Soundtrack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundtrack : MonoBehaviour
+{
+    public AudioClip[] Soundtracks;
+    void Start()
+    {
+        GetComponent<AudioSource>().clip = Soundtracks[Random.Range(0, Soundtracks.Length)];
+        GetComponent<AudioSource>().Play();
+    }
+}
